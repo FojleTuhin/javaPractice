8c28e2975c443e7dd2541c349fd8d5679fe04140
fojletuhin <fojletuhin@gmail.com>
2024-02-24 15:28:30 +0600

    add

--- java.js
+++ java.js
@@ -124,3 +124,21 @@ p='React';
 
 const isTrue=true;
 // console.log(!isTrue ? "hello" : "world")
+
+const sum =(p, q)=> {
+  p + q;
+ }
+ const result = sum(2, 3);
+//  console.log(result);
+
+if ("2" === 2) {
+  // console.log("Inside if");
+ } else {
+  // console.log("Inside else");
+ }
+
+
+ function work(x, y = 4) {
+  return x + y;
+ }
+ console.log(work(32);
