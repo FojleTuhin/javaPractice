258636165875fd5b9d75a1f7aad2df0437a15833
fojletuhin <fojletuhin@gmail.com>
2024-02-27 00:32:27 +0600

    add function

--- fetch.js
+++ fetch.js
@@ -19,3 +19,21 @@
         .then(res => res.json())
         .then(data => console.log(data))
     }
+
+    function loadData2(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(Response => Response.json())
+        .then(data => console.log(data))
+    }
+
+    function loadData3(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(Response => Response.json())
+        .then(data => loadInfo(data))
+    }
+
+    function loadInfo(data){
+        for(let user of data){
+            console.log(user.name)
+        }
+    }
