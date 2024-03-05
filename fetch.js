43375089918383cd9c089c754b774bbc33a5b8d9
fojletuhin <fojletuhin@gmail.com>
2024-03-05 20:50:11 +0600

    localStorage

--- fetch.js
+++ fetch.js
@@ -111,5 +111,12 @@ const myLoader = () =>{
 
 
 
+async function loading(){
+    const res =await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    const data = await res.json();
+    console.log(data)
+}
+
+loading()
 
 
