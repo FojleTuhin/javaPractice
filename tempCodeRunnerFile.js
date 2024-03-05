c2c9f1451fd0560a2d23aac39c2ecaca52176b95
fojletuhin <fojletuhin@gmail.com>
2024-03-05 21:09:03 +0600

    add localStorage

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,2 +1,17 @@
 
 
+
+const addProduct = () =>{
+    const productName= document.getElementById('Product-name');
+    const productQuantity = document.getElementById('Product-quantity');
+
+    
+
+    const nameValue= productName.value;
+    const quantityValue= productQuantity.value;
+
+   productName.value="";
+   productQuantity.value="";
+
+    console.log(nameValue, quantityValue)
+}
