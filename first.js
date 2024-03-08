daf319b6ca7f455a80c6be1dc44fb780fcac4a30
fojletuhin <fojletuhin@gmail.com>
2024-03-08 14:28:44 +0600

    spread operator

--- first.js
+++ first.js
@@ -1,26 +1,26 @@
 console.log(789);
 var seen =12;
-console. log( typeof seen);
+// console. log( typeof seen);
 var dream= 2343;
-console. log( typeof dream);
+// console. log( typeof dream);
  
 var name= 'fojle tuhin';
-console. log(typeof name);
+// console. log(typeof name);
 
 var hot= true;
 var cool= false;
 
-console. log( typeof hot);
+// console. log( typeof hot);
 var FirstName= `FOJLE`;
-console.log(FirstName);
-console.log(FirstName.toLowerCase());
-console.log(name. toUpperCase());
+// console.log(FirstName);
+// console.log(FirstName.toLowerCase());
+// console.log(name. toUpperCase());
 
-console.log(name.indexOf('tuhin'));
+// console.log(name.indexOf('tuhin'));
 
 var list1= 5.8235468;
 list1= Math.ceil(list1);
-console.log(list1);
+// console.log(list1);
 
 
 
@@ -28,30 +28,30 @@ console.log(list1);
 var price1= 45;
 var price2=34;
 var total = price1 / price2;
-console. log(total);
+// console. log(total);
 
 var list2= 38.454
 list2= Math.floor(list2);
-console.log(list2);
+// console.log(list2);
 
 var list3= 4;
 var list4= '5';
 list4= +list4;
 var totalList= list3+ list4;
-console.log(totalList);
+// console.log(totalList);
 
 var item=45;
 if(item>=50){
-    console.log("give me this item.");
+    // console.log("give me this item.");
 }
 
 else{
-    console.log('dont need this');
+    // console.log('dont need this');
 }
 
 
 var date =new Date('2022-12-01');
-console.log(date);
+// console.log(date);
 
 
 var text1= "f0jle";
@@ -59,8 +59,18 @@ var text2= "tuhin";
 
 
 var text = text2.concat(text1);
-console.log(text);
+// console.log(text);
 
 
 text= text1.slice(1,3);
-console.log(text);
+// console.log(text);
+
+
+const num= [1,2,3,4,5];
+
+const newnum=[...num,7,8];
+newnum.push(6)
+
+
+console.log(num)
+console.log(newnum)
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -14,4 +14,46 @@ const addProduct = () =>{
    productQuantity.value="";
 
     console.log(nameValue, quantityValue)
+
+    displayProduct(nameValue, quantityValue)
+    saveProductToLocalStorage(nameValue, quantityValue)
 }
+
+
+const displayProduct= (nameValue, quantityValue) =>{
+
+    const ul=document.getElementById('product-container');
+    const li= document.createElement('li')
+
+    li.innerText= `${ nameValue} : ${quantityValue}`;
+    ul.appendChild(li)
+
+}
+
+const getStoredShoppingCard = () =>{
+    let cart={};
+    const storedCart =localStorage.getItem('cart')
+    if(storedCart){
+        cart=JSON.parse(storedCart);
+    }
+    return cart;
+} 
+
+const saveProductToLocalStorage = (nameValue, quantityValue) =>{
+    const cart = getStoredShoppingCard();
+    cart[nameValue]=quantityValue;
+    const cartStringField= JSON.stringify(cart);
+    localStorage.setItem('cart', cartStringField)
+}
+
+const displayProductsFromLocalStorage= ()=>{
+    const savedCart =getStoredShoppingCard();
+    console.log(savedCart);
+    for(const product in savedCart){
+        const quantity = savedCart[product];
+        console.log(product, quantity )
+        displayProduct(product, quantity)
+    }
+}
+displayProductsFromLocalStorage();
+
